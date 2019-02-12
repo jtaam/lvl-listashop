00795c516eeb5ba86b7cb31df0a7da98032bdad0
_tam <tamjid84@yahoo.com>
2019-02-13 02:10:54 +0600

    Latest posts - latest()

--- app/Http/Controllers/frontend/website/HomePageController.php
+++ app/Http/Controllers/frontend/website/HomePageController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 class HomePageController extends Controller
 {
     public function home(){
-        $latestProducts = Product::orderBy('id', 'desc')->take(8)->get();
+        $latestProducts = Product::latest()->take(8)->get();
         return view('frontend.website.index', compact('latestProducts'));
     }
 }
